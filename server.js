55a318ab52d77d10c48a6f61a999f04c4c1a6703
Sean Blevins <seanbinvt@users.noreply.github.com>
2020-07-31 04:50:32 -0400

    added node and npm engines to package

--- server.js
+++ server.js
@@ -2,14 +2,17 @@ const express = require('express');
 const http = require('http');
 const path = require('path');
 let app = express();
-const port = process.env.PORT || '8080';
+const port = process.env.PORT || '3000';
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-}
 
-app.set('port', port);
-app.get('*', (request, response) => {
-    response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+app.use(express.static('/build'));
+
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-console.log(`Running on :${port}`)
+
+app.listen(port)
